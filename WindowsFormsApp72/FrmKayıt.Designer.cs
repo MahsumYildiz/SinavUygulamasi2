a1b90eb84c2c0f409dc2b87f11e6743c0dcf2a0e
MahsumYildiz <mbtkral63@gmail.com>
2022-05-18 15:07:42 +0300

    add register form

---
+++ WindowsFormsApp72/FrmKayıt.Designer.cs
@@ -0,0 +1,372 @@
+﻿
+namespace WindowsFormsApp72
+{
+    partial class FrmKayıt
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmKayıt));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Txt_Adi = new System.Windows.Forms.TextBox();
+            this.Txt_Soyadi = new System.Windows.Forms.TextBox();
+            this.Txt_Kadi = new System.Windows.Forms.TextBox();
+            this.Txt_Sifre = new System.Windows.Forms.TextBox();
+            this.Txt_Mail = new System.Windows.Forms.TextBox();
+            this.grbxOgr = new System.Windows.Forms.GroupBox();
+            this.Btn_OgrKayit = new System.Windows.Forms.Button();
+            this.grbxSinavS = new System.Windows.Forms.GroupBox();
+            this.btnSinavSKayit = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Txt_Mail2 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.Txt_Sifre2 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.Txt_Kadi2 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.Txt_Soyadi2 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.Txt_Adi2 = new System.Windows.Forms.TextBox();
+            this.BtnGeri = new System.Windows.Forms.Button();
+            this.grbxOgr.SuspendLayout();
+            this.grbxSinavS.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(44, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Adı";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.Location = new System.Drawing.Point(44, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Soyadı";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.Location = new System.Drawing.Point(44, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Mail";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label4.Location = new System.Drawing.Point(44, 168);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Şifre";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label5.Location = new System.Drawing.Point(44, 205);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(93, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Kullanıcı Adı";
+            // 
+            // Txt_Adi
+            // 
+            this.Txt_Adi.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.Txt_Adi.Location = new System.Drawing.Point(185, 44);
+            this.Txt_Adi.Name = "Txt_Adi";
+            this.Txt_Adi.Size = new System.Drawing.Size(106, 23);
+            this.Txt_Adi.TabIndex = 5;
+            // 
+            // Txt_Soyadi
+            // 
+            this.Txt_Soyadi.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.Txt_Soyadi.Location = new System.Drawing.Point(185, 83);
+            this.Txt_Soyadi.Name = "Txt_Soyadi";
+            this.Txt_Soyadi.Size = new System.Drawing.Size(106, 23);
+            this.Txt_Soyadi.TabIndex = 6;
+            // 
+            // Txt_Kadi
+            // 
+            this.Txt_Kadi.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.Txt_Kadi.Location = new System.Drawing.Point(185, 210);
+            this.Txt_Kadi.Name = "Txt_Kadi";
+            this.Txt_Kadi.Size = new System.Drawing.Size(106, 23);
+            this.Txt_Kadi.TabIndex = 7;
+            // 
+            // Txt_Sifre
+            // 
+            this.Txt_Sifre.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.Txt_Sifre.Location = new System.Drawing.Point(185, 168);
+            this.Txt_Sifre.Name = "Txt_Sifre";
+            this.Txt_Sifre.Size = new System.Drawing.Size(106, 23);
+            this.Txt_Sifre.TabIndex = 8;
+            // 
+            // Txt_Mail
+            // 
+            this.Txt_Mail.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.Txt_Mail.Location = new System.Drawing.Point(185, 130);
+            this.Txt_Mail.Name = "Txt_Mail";
+            this.Txt_Mail.Size = new System.Drawing.Size(106, 23);
+            this.Txt_Mail.TabIndex = 9;
+            // 
+            // grbxOgr
+            // 
+            this.grbxOgr.Controls.Add(this.Btn_OgrKayit);
+            this.grbxOgr.Controls.Add(this.label1);
+            this.grbxOgr.Controls.Add(this.Txt_Mail);
+            this.grbxOgr.Controls.Add(this.label2);
+            this.grbxOgr.Controls.Add(this.Txt_Sifre);
+            this.grbxOgr.Controls.Add(this.label3);
+            this.grbxOgr.Controls.Add(this.Txt_Kadi);
+            this.grbxOgr.Controls.Add(this.label4);
+            this.grbxOgr.Controls.Add(this.Txt_Soyadi);
+            this.grbxOgr.Controls.Add(this.label5);
+            this.grbxOgr.Controls.Add(this.Txt_Adi);
+            this.grbxOgr.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.grbxOgr.Location = new System.Drawing.Point(24, 67);
+            this.grbxOgr.Name = "grbxOgr";
+            this.grbxOgr.Size = new System.Drawing.Size(361, 338);
+            this.grbxOgr.TabIndex = 10;
+            this.grbxOgr.TabStop = false;
+            this.grbxOgr.Text = "Öğrenci Kayıt";
+            // 
+            // Btn_OgrKayit
+            // 
+            this.Btn_OgrKayit.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.Btn_OgrKayit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_OgrKayit.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.Btn_OgrKayit.ImageKey = "sig.jpg";
+            this.Btn_OgrKayit.Location = new System.Drawing.Point(105, 254);
+            this.Btn_OgrKayit.Name = "Btn_OgrKayit";
+            this.Btn_OgrKayit.Size = new System.Drawing.Size(120, 56);
+            this.Btn_OgrKayit.TabIndex = 10;
+            this.Btn_OgrKayit.Text = "Kayıt";
+            this.Btn_OgrKayit.UseVisualStyleBackColor = false;
+            this.Btn_OgrKayit.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // grbxSinavS
+            // 
+            this.grbxSinavS.Controls.Add(this.btnSinavSKayit);
+            this.grbxSinavS.Controls.Add(this.label6);
+            this.grbxSinavS.Controls.Add(this.Txt_Mail2);
+            this.grbxSinavS.Controls.Add(this.label7);
+            this.grbxSinavS.Controls.Add(this.Txt_Sifre2);
+            this.grbxSinavS.Controls.Add(this.label8);
+            this.grbxSinavS.Controls.Add(this.Txt_Kadi2);
+            this.grbxSinavS.Controls.Add(this.label9);
+            this.grbxSinavS.Controls.Add(this.Txt_Soyadi2);
+            this.grbxSinavS.Controls.Add(this.label10);
+            this.grbxSinavS.Controls.Add(this.Txt_Adi2);
+            this.grbxSinavS.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.grbxSinavS.Location = new System.Drawing.Point(423, 67);
+            this.grbxSinavS.Name = "grbxSinavS";
+            this.grbxSinavS.Size = new System.Drawing.Size(361, 338);
+            this.grbxSinavS.TabIndex = 11;
+            this.grbxSinavS.TabStop = false;
+            this.grbxSinavS.Text = "Sınav Sorumlusu Kayıt";
+            // 
+            // btnSinavSKayit
+            // 
+            this.btnSinavSKayit.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnSinavSKayit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSinavSKayit.Location = new System.Drawing.Point(125, 254);
+            this.btnSinavSKayit.Name = "btnSinavSKayit";
+            this.btnSinavSKayit.Size = new System.Drawing.Size(120, 56);
+            this.btnSinavSKayit.TabIndex = 11;
+            this.btnSinavSKayit.Text = "Kayıt";
+            this.btnSinavSKayit.UseVisualStyleBackColor = false;
+            this.btnSinavSKayit.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label6.Location = new System.Drawing.Point(44, 47);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(32, 20);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Adı";
+            // 
+            // Txt_Mail2
+            // 
+            this.Txt_Mail2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.Txt_Mail2.Location = new System.Drawing.Point(185, 130);
+            this.Txt_Mail2.Name = "Txt_Mail2";
+            this.Txt_Mail2.Size = new System.Drawing.Size(106, 23);
+            this.Txt_Mail2.TabIndex = 9;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label7.Location = new System.Drawing.Point(44, 83);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(57, 20);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "Soyadı";
+            // 
+            // Txt_Sifre2
+            // 
+            this.Txt_Sifre2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.Txt_Sifre2.Location = new System.Drawing.Point(185, 210);
+            this.Txt_Sifre2.Name = "Txt_Sifre2";
+            this.Txt_Sifre2.Size = new System.Drawing.Size(106, 23);
+            this.Txt_Sifre2.TabIndex = 8;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label8.Location = new System.Drawing.Point(44, 130);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(37, 20);
+            this.label8.TabIndex = 2;
+            this.label8.Text = "Mail";
+            // 
+            // Txt_Kadi2
+            // 
+            this.Txt_Kadi2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.Txt_Kadi2.Location = new System.Drawing.Point(185, 172);
+            this.Txt_Kadi2.Name = "Txt_Kadi2";
+            this.Txt_Kadi2.Size = new System.Drawing.Size(106, 23);
+            this.Txt_Kadi2.TabIndex = 7;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label9.Location = new System.Drawing.Point(44, 208);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(42, 20);
+            this.label9.TabIndex = 3;
+            this.label9.Text = "Şifre";
+            // 
+            // Txt_Soyadi2
+            // 
+            this.Txt_Soyadi2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.Txt_Soyadi2.Location = new System.Drawing.Point(185, 83);
+            this.Txt_Soyadi2.Name = "Txt_Soyadi2";
+            this.Txt_Soyadi2.Size = new System.Drawing.Size(106, 23);
+            this.Txt_Soyadi2.TabIndex = 6;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label10.Location = new System.Drawing.Point(44, 168);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(93, 20);
+            this.label10.TabIndex = 4;
+            this.label10.Text = "Kullanıcı Adı";
+            // 
+            // Txt_Adi2
+            // 
+            this.Txt_Adi2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.Txt_Adi2.Location = new System.Drawing.Point(185, 44);
+            this.Txt_Adi2.Name = "Txt_Adi2";
+            this.Txt_Adi2.Size = new System.Drawing.Size(106, 23);
+            this.Txt_Adi2.TabIndex = 5;
+            // 
+            // BtnGeri
+            // 
+            this.BtnGeri.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.BtnGeri.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("BtnGeri.BackgroundImage")));
+            this.BtnGeri.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.BtnGeri.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnGeri.Location = new System.Drawing.Point(24, 17);
+            this.BtnGeri.Name = "BtnGeri";
+            this.BtnGeri.Size = new System.Drawing.Size(54, 44);
+            this.BtnGeri.TabIndex = 12;
+            this.BtnGeri.UseVisualStyleBackColor = false;
+            this.BtnGeri.Click += new System.EventHandler(this.BtnGeri_Click);
+            // 
+            // FrmKayıt
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(830, 450);
+            this.Controls.Add(this.BtnGeri);
+            this.Controls.Add(this.grbxSinavS);
+            this.Controls.Add(this.grbxOgr);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmKayıt";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kayıt";
+            this.grbxOgr.ResumeLayout(false);
+            this.grbxOgr.PerformLayout();
+            this.grbxSinavS.ResumeLayout(false);
+            this.grbxSinavS.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox Txt_Adi;
+        private System.Windows.Forms.TextBox Txt_Soyadi;
+        private System.Windows.Forms.TextBox Txt_Kadi;
+        private System.Windows.Forms.TextBox Txt_Sifre;
+        private System.Windows.Forms.TextBox Txt_Mail;
+        private System.Windows.Forms.GroupBox grbxOgr;
+        private System.Windows.Forms.GroupBox grbxSinavS;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox Txt_Mail2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox Txt_Sifre2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox Txt_Kadi2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox Txt_Soyadi2;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button Btn_OgrKayit;
+        private System.Windows.Forms.Button btnSinavSKayit;
+        private System.Windows.Forms.TextBox Txt_Adi2;
+        private System.Windows.Forms.Button BtnGeri;
+    }
+}
---
+++ WindowsFormsApp72/FrmKayıt.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace WindowsFormsApp72
+{
+    public partial class FrmKayıt : Form
+    {
+        SqlConnection baglanti = new SqlConnection("data source=DESKTOP-2EB8G19;initial catalog=YazilimYapimiProje; integrated security=true;");
+        public FrmKayıt()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) //öğrencinin kayıt edilmesi
+        {
+            baglanti.Open();
+            SqlCommand sqlKayit = new SqlCommand("Insert into Tbl_Ogrenci(OgrenciAd,OgrenciSoyad,OgrenciMail,OgrenciSifre,OgrenciKadi) Values('"+Txt_Adi.Text.ToString()+ "','"+Txt_Soyadi.Text.ToString()+ "' ,'" + Txt_Mail.Text.ToString() + "','" + Txt_Sifre.Text.ToString() + "' ,'" + Txt_Kadi.Text.ToString() + "')" ,baglanti );
+            sqlKayit.ExecuteNonQuery();
+            baglanti.Close();
+            MessageBox.Show("Öğrenci Kaydı Başarılı");
+
+        }
+
+        private void button2_Click(object sender, EventArgs e) //sınav sorumlusu kayıt işlemi
+        {
+            baglanti.Open();
+            SqlCommand sqlkayit = new SqlCommand("Insert into Tbl_SınavS(SınavSAd,SınavSSoyad,SınavSMail,SınavSKadi,SınavSSifre) Values('" + Txt_Adi2.Text.ToString() + "','" + Txt_Soyadi2.Text.ToString() + "' ,'" + Txt_Mail2.Text.ToString() + "','" + Txt_Kadi2.Text.ToString() + "' ,'" + Txt_Sifre2.Text.ToString() + "')", baglanti);
+            sqlkayit.ExecuteNonQuery();
+            baglanti.Close();
+            MessageBox.Show("Sınav Sorumlusu Kaydı Başarılı");
+        }
+
+        private void BtnGeri_Click(object sender, EventArgs e)
+        {
+            FrmAnaSayfa frmAnaSayfa = new FrmAnaSayfa();
+            frmAnaSayfa.Show();
+            this.Hide();
+        }
+    }
+}
